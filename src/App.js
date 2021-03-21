0e2d4988aae9301d5811f35a50f387253306fe7e
James Prickitt <jwprickitt@gmail.com>
2021-03-21 15:50:54 -0400

    started to create components but spins in browser

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React, { Component } from "react";
-import { Route, Switch, withRouter } from "react-router-dom";
-import logo from "./logo.svg";
+// import { Route, Switch, withRouter } from "react-router-dom";
+// import SharedHeader from "./Components/SharedComponents/SharedHeader";
+// import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        {/* <SharedHeader /> */}
         {/* <!--make a set of registration components--> */}
 
         {/* <!--make a global registration header include--> */}
         {/* <!--need components for reg form elements--> */}
         {/* <!--make different components for demo form elements --> */}
         {/* <!--make different components for item form elements --> */}
-        {/* <!--should define fields by sitetypes--> */}
-
-        <header className="App-header">
+        {/* <!--LATER: should define fields by sitetypes--> */}
+        {/* <header className="App-header">
           <a className="home-link" href="/">
             <img src={logo} className="App-logo" alt="logo" />
           </a>
           <p>Welcome to Event Registration</p>
-        </header>
+        </header> */}
         <section>
           {/* <!--need a link (that looks like a button?) : Register as an Attendee--> */}
 
           {/* <!--need a route: /register/reginfo/ -- make this have logic for any kind of attendee and be dynamic, but based on what? localStorage probably --> */}
           <a className="whatever" href="/register/reginfo/">
-            Register as an Attendee
+            Register as an Attendee :)
           </a>
         </section>
         {/* <!--make a global registration footer include--> */}
---
+++ src/Components/SharedComponents/SharedHeader.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import logo from "../../logo.svg";
+
+const SharedHeader = () => {
+  return (
+    <SharedHeader>
+      some text
+      {/* <header className="App-header">
+        <a className="home-link" href="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </a>
+        <p>Welcome to Event Registration</p>
+      </header>
+      ; */}
+    </SharedHeader>
+  );
+};
+
+export default SharedHeader;
