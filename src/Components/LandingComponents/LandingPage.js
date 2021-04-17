5ac3ce58963f70bc6a4afb438bec1931e179f6dc
James Prickitt <jwprickitt@gmail.com>
2021-04-17 17:15:42 -0400

    readme update

--- src/Components/LandingComponents/LandingPage.js
+++ src/Components/LandingComponents/LandingPage.js
@@ -6,15 +6,45 @@ const LandingPage = () => {
     <section>
       <section id="admin">
         <ul>
-          <li>
-            <a href="/admin/">Event Admin</a>
-          </li>
+          <li>Event Admin (separate app, TBD)</li>
         </ul>
       </section>
       <section id="registration">
+        <p>(or see a list of events and button for register)</p>
         <ul>
           <li>
-            <a href="/register/reginfo/">Register as an Attendee</a>
+            Event 1:
+            <ul>
+              <li>
+                <a href="/reg/attendee/start">Register as an Attendee</a>
+              </li>
+              <li>
+                <a href="/reg/attendee/admin">Attendee Admin</a>
+              </li>
+              <li>
+                <a href="/reg/attendee/upgrade">Attendee Upgrade</a>
+              </li>
+              <li>
+                <a href="/reg/exhibitor/start">Register as an Exhibitor</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            Event 2:
+            <ul>
+              <li>
+                <a href="/reg/attendee/start">Register as an Attendee</a>
+              </li>
+              <li>
+                <a href="/reg/attendee/admin">Attendee Admin</a>
+              </li>
+              <li>
+                <a href="/reg/attendee/upgrade">Attendee Upgrade</a>
+              </li>
+              <li>
+                <a href="/reg/exhibitor/start">Register as an Exhibitor</a>
+              </li>
+            </ul>
           </li>
         </ul>
       </section>
